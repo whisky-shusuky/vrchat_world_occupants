36b498400abe71092d49c2ceaae331a18fd3ce73
whisky-shunsuke <whisky.shusuky@gmail.com>
2018-09-17 19:53:44 +0900

    git init Add simple get target url.

---
+++ get_world_visitors.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+
+
+
+def get_json(location, limit = 10)
+  raise ArgumentError, 'too many HTTP redirects' if limit == 0
+  uri = URI.parse(location)
+  begin
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.open_timeout = 5
+      http.read_timeout = 10
+      http.get(uri.request_uri)
+    end
+    case response
+    when Net::HTTPSuccess
+      json = response.body
+      JSON.parse(json)
+    when Net::HTTPRedirection
+      location = response['location']
+      warn "redirected to #{location}"
+      get_json(location, limit - 1)
+    else
+      puts [uri.to_s, response.value].join(" : ")
+      # handle error
+    end
+  rescue => e
+    puts [uri.to_s, e.class, e].join(" : ")
+    # handle error
+  end
+end
+
+puts get_json('https://vrchat.net/api/1/worlds/wrld_12740759-476d-41f1-92aa-6855e997837c?apiKey=JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26')
+
